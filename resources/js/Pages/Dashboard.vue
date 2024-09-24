b3db20238c32633946cd0cde2b3c2091584e75e6
Marat Mustafin <ardislamov.m@gmail.com>
2024-09-24 07:55:07 +0300

    TASK-1
    Signed-off-by: Marat Mustafin <ardislamov.m@gmail.com>

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,17 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
+// import { QrcodeStream } from 'vue-qrcode-reader';
+
+import VueQrcodeReader from 'vue-qrcode-reader';
+
+Vue.use(VueQrcodeReader)
+
+const app = new Vue({
+    el: '#app',
+    components: { App },
+});
+
 </script>
 
 <template>
@@ -15,6 +26,15 @@ import { Head } from '@inertiajs/vue3';
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">You're logged in!</div>
+                    <div class="flex">
+                        <div class="flex-initial w-64 ...">
+                            <div class="grid-cols-1">
+                                <qrcode-stream></qrcode-stream>
+
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
