9cd5ec337e4a94e30fba1ea1a009cd5476269ef5
Marat Mustafin <ardislamov.m@gmail.com>
2024-09-24 10:32:52 +0300

    TASK-1
    * frontend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,6 +4,7 @@ const axios = Axios.create({
     baseURL: import.meta.env.VITE_PUBLIC_BACKEND_URL,
     headers: {
         'X-Requested-With': 'XMLHttpRequest',
+        'Accept' : 'aplication/json',
     },
     withCredentials: true,
     withXSRFToken: true
